f8793ac042446fd662c636e5884b4eb703791cb7
FutRR <futterermaxime@gmail.com>
2024-03-29 15:11:49 +0100

    added bs style to main page

--- view/layout.php
+++ view/layout.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -11,11 +11,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
         integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
     <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css
+">
     <title>FORUM</title>
 </head>
 
 <body>
-    <div id="wrapper">
+    <div class="m-5" id="wrapper">
         <div id="mainpage">
             <!-- c'est ici que les messages (erreur ou succès) s'affichent-->
             <h3 class="message" style="color: red">
@@ -27,11 +29,12 @@
             <header>
                 <nav>
                     <div id="nav-left">
-                        <a href="/">Accueil</a>
+                        <a class="btn btn-primary m-1" href="index.php">Accueil</a>
                         <?php
                         if (App\Session::isAdmin()) {
                             ?>
-                            <a href="index.php?ctrl=home&action=users">Voir la liste des gens</a>
+                            <a class="btn btn-primary m-1" href="index.php?ctrl=home&action=users">Voir la liste des
+                                gens</a>
                         <?php } ?>
                     </div>
                     <div id="nav-right">
@@ -39,16 +42,18 @@
                         // si l'utilisateur est connecté 
                         if (App\Session::getUser()) {
                             ?>
-                            <a href="index.php?ctrl=security&action=profile"><span class="fas fa-user"></span>&nbsp;
+                            <a class="btn btn-primary" href="index.php?ctrl=security&action=profile"><span
+                                    class="fas fa-user"></span>&nbsp;
                                 <?= App\Session::getUser() ?>
                             </a>
-                            <a href="index.php?ctrl=security&action=logout">Déconnexion</a>
+                            <a class="btn btn-secondary m-1" href="index.php?ctrl=security&action=logout">Déconnexion</a>
                             <?php
                         } else {
                             ?>
-                            <a href="index.php?ctrl=security&action=login">Connexion</a>
-                            <a href="index.php?ctrl=security&action=register">Inscription</a>
-                            <a href="index.php?ctrl=forum&action=index">Liste des catégories</a>
+                            <a class="btn btn-secondary m-1" href="index.php?ctrl=security&action=login">Connexion</a>
+                            <a class="btn btn-secondary m-1" href="index.php?ctrl=security&action=register">Inscription</a>
+                            <a class="btn btn-secondary m-1" href="index.php?ctrl=forum&action=index">Liste des
+                                catégories</a>
                             <?php
                         }
                         ?>
@@ -62,7 +67,8 @@
         </div>
         <footer>
             <p>&copy;
-                <?= date_create("now")->format("Y") ?> - <a href="#">Règlement du forum</a> - <a href="#">Mentions
+                <?= date_create("now")->format("Y") ?> - <a class="p-1 rounded" href="#">Règlement du forum</a>
+                - <a class="p-1 rounded" href="#">Mentions
                     légales</a>
             </p>
         </footer>
